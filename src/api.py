a6a91deef4f2258d86bb3825ff7efc814048f091
Douglas Reid <douglas-reid@users.noreply.github.com>
2022-11-03 08:59:57 -0700

    refactor: use latest version of python-client (#5)

--- src/api.py
+++ src/api.py
@@ -1,25 +1,24 @@
 """Package that generates Markdown transcriptions of audio with inline formatting."""
 import base64
 import json
-import logging
-import pathlib
+from pathlib import Path
 from typing import Optional, Type
 
 import requests
 import toml
 from pydantic import HttpUrl
-from steamship import File, MimeTypes, PluginInstance
-from steamship.app import App, Response, create_handler, post
+from steamship import File, MimeTypes
 from steamship.base import Task, TaskState
-from steamship.plugin.config import Config
+from steamship.invocable import Config, Invocable, InvocableResponse, create_handler, post
 
 from transcript_to_markdown import transcript_to_markdown
 
 
-class AudioMarkdownPackage(App):
+class AudioMarkdownPackage(Invocable):
     """Package that transcribes audio to Markdown using in-audio formatting cues."""
 
-    BLOCKIFIER_HANDLE = "whisper-s2t-blockifier"
+    BLOCKIFIER_HANDLE = "whisper-s2t-blockifier-staging"
+    BLOCKIFIER_INSTANCE_HANDLE = "whisper-s2t-blockifier-staging-instance-001"
 
     class AudioMarkdownPackageConfig(Config):
         """Config object containing required configuration parameters to initialize a AudioMarkdownPackage."""
@@ -31,54 +30,72 @@ class AudioMarkdownPackage(App):
         return self.AudioMarkdownPackageConfig
 
     def __init__(self, **kwargs):
-        secret_kwargs = toml.load(
-            str(pathlib.Path(__file__).parent / ".steamship" / "secrets.toml")
-        )
+        secret_kwargs = toml.load(str(Path(__file__).parent / ".steamship" / "secrets.toml"))
         kwargs["config"] = {
             **secret_kwargs,
             **{k: v for k, v in kwargs["config"].items() if v != ""},
         }
         super().__init__(**kwargs)
 
-        self.s2t_blockifier = PluginInstance.create(
-            self.client,
+        # todo(douglas-reid): does hard-coding the instance handle make this brittle per-workspace?
+        self.s2t_blockifier = self.client.use_plugin(
             plugin_handle=self.BLOCKIFIER_HANDLE,
-            config={},
-        ).data
+            instance_handle=self.BLOCKIFIER_INSTANCE_HANDLE,
+            config={"whisper_model": "base", "get_segments": False},
+            reuse=True,
+        )
 
     @post("transcribe_url")
-    def transcribe_url(self, url: HttpUrl, mime_type: Optional[MimeTypes] = None) -> Response:
+    def transcribe_url(
+        self, url: HttpUrl, mime_type: Optional[MimeTypes] = None
+    ) -> InvocableResponse:
         """Transcribe audio from URL."""
-        mime_type = mime_type or MimeTypes.MP3
-        file = File.create(self.client, content=requests.get(url).content, mime_type=mime_type).data
+        file = File.create(
+            self.client, content=requests.get(url).content, mime_type=(mime_type or MimeTypes.MP3)
+        )
         return self._transcribe_audio_file(file)
 
     @post("transcribe")
-    def transcribe(self, audio: str, mime_type: str = MimeTypes.MP3) -> Response:
+    def transcribe(self, encoded_audio: str, mime_type: str = MimeTypes.MP3) -> InvocableResponse:
         """Summarize audio using AssemblyAI."""
-        audio = base64.b64decode(audio.encode("utf-8"))
-        file = File.create(self.client, content=audio, mime_type=mime_type).data
+        raw_audio = base64.b64decode(encoded_audio)
+        file = File.create(self.client, content=raw_audio, mime_type=(mime_type or MimeTypes.MP3))
         return self._transcribe_audio_file(file)
 
     @post("get_markdown")
     def get_markdown(self, task_id: str):
         """Get the markdown for a transcribed audio file based on task_id."""
-        task = Task.get(self.client, _id=task_id).data
+        task = Task.get(self.client, _id=task_id)
         if task.state != TaskState.succeeded:
-            return Response(json={"task_id": task.task_id, "status": task.state})
-        else:
-            file_id = json.loads(task.input)["id"]
-            file = File.get(self.client, file_id).data
-            transcript_text = file.blocks[0].text
-            logging.debug(f"transcription text: {transcript_text}")
-            markdown_text = transcript_to_markdown(transcript_text)
-            logging.debug(f"markdown text: {transcript_text}")
-            return Response(json={"markdown": markdown_text, "status": task.state})
+            return InvocableResponse(
+                json={
+                    "task_id": task.task_id,
+                    "status": task.state,
+                    "status_message": task.status_message,
+                }
+            )
 
-    def _transcribe_audio_file(self, file) -> Response:
-        status = file.blockify(plugin_instance=self.s2t_blockifier.handle)
-        task = status.task
-        return Response(json={"task_id": task.task_id, "status": task.state})
+        file_id = json.loads(task.input)["id"]
+        file = File.get(self.client, file_id)
+        transcript_text = file.blocks[0].text
+        markdown_text = transcript_to_markdown(transcript_text)
+        return InvocableResponse(
+            json={
+                "markdown": markdown_text,
+                "status": task.state,
+                "status_message": task.status_message,
+            }
+        )
+
+    def _transcribe_audio_file(self, file) -> InvocableResponse:
+        task = file.blockify(plugin_instance=self.s2t_blockifier.handle)
+        return InvocableResponse(
+            json={
+                "task_id": task.task_id,
+                "status": task.state,
+                "status_message": task.status_message,
+            }
+        )
 
 
 handler = create_handler(AudioMarkdownPackage)
---
+++ tests/data/output.md
@@ -0,0 +1,18 @@
+# testing this Deam Ship Audio Markdown Package 
+
+Here's a list of things we'd like to check, 
+1. whisper model, 
+2. markdown extraction, 
+3. webpage, 
+
+Here's a list of things we don't care about, 
+* misspellings, 
+* weird grammar, 
+# this is the first heading 
+## this is the second heading 
+### this is the third heading 
+#### this is the fourth heading 
+##### this is the sixth element. 
+###### this is the sixth heading 
+
+Thank you for listening to our test.
